67e96a5aeb7deaeb0f16fc41edef0c6a5fd0e002
Anh Trinh <anh@catalyst.net.nz>
2015-02-10 18:30:05 +1300

    try SC

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,46 +1,64 @@
 (function() {
 
 	'use strict';
-	
+
 	var soundCloudify = angular.module('soundCloudify', []);
 
 	soundCloudify.value('CLIENT_ID', '849e84ac5f7843ce1cbc0e004ae4fb69');
 
 	soundCloudify.service('SoundCloudService', function($http, CLIENT_ID) {
 
+		var getTracks = function() {
+			return $http.get('https://api.soundcloud.com/tracks?client_id=' + CLIENT_ID);
+		};
+
 		var getTrack = function() {
-			return $http.get('http://api.soundcloud.com/tracks/13158665.json?client_id=' + CLIENT_ID);
+			return $http.get('https://api.soundcloud.com/tracks/13158665.json?client_id=' + CLIENT_ID);
 		};
 
 		return {
-			getTrack: getTrack
+			getTrack: getTrack,
+			getTracks: getTracks
 		};
 
 	});
 
-	soundCloudify.controller('MainController', function(SoundCloudService, CLIENT_ID) {
+	soundCloudify.service('SDK', function($q, CLIENT_ID) {
+		SC.initialize({
+		  client_id: CLIENT_ID
+		});
+
+		var play = function() {
+			
+		};
+
+		var getTracks = function() {
+			var d = $q.defer();
+			SC.get('/tracks', function(tracks, error) {
+				d.resolve(tracks);
+			});
+
+			return d.promise;
+		};
+
+		return {
+			play: play,
+			getTracks: getTracks
+		}
+
+	});
+
+	soundCloudify.controller('MainController', function(SDK, CLIENT_ID) {
 
 		var self = this;
 
-		this.getTrack = function() {
-			SoundCloudService.getTrack().success(function(track) {
-				self.track = track;
-
-				console.log("Start Playing");
-				soundManager.createSound({
-					id: 'mySound',
-					url: track.stream_url + '?client_id=' + CLIENT_ID,
-					autoLoad: true,
-					autoPlay: true,
-					onload: function() {
-						console.log('The sound '+this.id+' loaded!');
-					},
-					volume: 50
-				});
+		this.getTracks = function() {
+			SDK.getTracks().then(function(tracks) {
+				self.tracks = tracks;
 			});
 		};
 
+		this.getTracks();
 	});
-
 
 }());
