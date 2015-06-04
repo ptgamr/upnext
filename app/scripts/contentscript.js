1ebf32b26798462ca5e9918f95e44dddce00d3d6
Anh Trinh <anh@catalyst.net.nz>
2015-06-04 14:53:55 +1200

    experiment with floating button

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,1 +1,36 @@
 'use strict';
+
+// var floatingButtonHtml = 
+//         '<ul class="mfb-component--br mfb-slidein" data-mfb-toggle="click">' +
+//             '<li class="mfb-component__wrap">' +
+//                 '<a href="#" class="mfb-component__button--main">' +
+//                   '<i class="mfb-component__main-icon--resting ion-plus-round"></i>' +
+//                   '<i class="mfb-component__main-icon--active ion-close-round"></i>' +
+//                 '</a>' +
+//                 '<ul class="mfb-component__list">' +
+//                   '<li>' +
+//                     '<a href="https://github.com/nobitagit/material-floating-button/" data-mfb-label="View on Github" class="mfb-component__button--child">' +
+//                       '<i class="mfb-component__child-icon ion-social-github"></i>' +
+//                     '</a>' +
+//                   '</li>' +
+//                   '<li>' +
+//                     '<a href="https://github.com/nobitagit" data-mfb-label="Follow me on Github" class="mfb-component__button--child">' +
+//                       '<i class="mfb-component__child-icon ion-social-octocat"></i>' +
+//                     '</a>' +
+//                   '</li>' +
+
+//                   '<li>' +
+//                     '<a href="http://twitter.com/share?text=Check this material floating button component!&url=http://nobitagit.github.io/material-floating-button/&hashtags=material,design,button,css"' +
+//                        'data-mfb-label="Share on Twitter" class="mfb-component__button--child">' +
+//                       '<i class="mfb-component__child-icon ion-social-twitter"></i>' +
+//                     '</a>' +
+//                   '</li>' +
+//                 '</ul>' +
+//             '</li>' +
+//         '</ul>';
+
+// var soundcloudifyElement = document.createElement('div');
+// soundcloudifyElement.setAttribute("id", "soundcloudify-floating-button");
+// soundcloudifyElement.innerHTML = floatingButtonHtml;
+
+// document.body.appendChild(soundcloudifyElement);
